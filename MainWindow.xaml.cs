836460479e233f89daddaebbaec81a35b8031934
jeffreyseneff <jeffreyseneff@JEFFREYSENE94EF>
2025-03-26 05:09:04 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace WPFTutorial
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tbHello.Text = "Hello from Code Behind!";
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
